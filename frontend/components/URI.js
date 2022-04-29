2bfb256f3210b31ca8aa9c682636892ac51fb722
heartandkarma <heartandkarma@gmail.com>
2022-04-29 11:18:21 -0700

    got login posting to localstorage

---
+++ frontend/components/URI.js
@@ -0,0 +1,9 @@
+export const postLogin = "http://localhost:9000/api/login";
+
+export const getArticle = "http://localhost:9000/api/articles";
+
+export const postArticle = "http://localhost:9000/api/articles";
+
+export const putArticle = "http://localhost:9000/api/articles/";
+
+export const deleteArticle = "http://localhost:9000/api/articles/";
